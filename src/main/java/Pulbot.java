63f53329472b90a5b0094877672a586e2222a0c2
Pulastya Satapathy <83450788+pulas2345@users.noreply.github.com>
2026-08-20 20:42:26 +0800

    Rename main class

--- src/main/java/Pulbot.java
+++ src/main/java/Pulbot.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Pulbot {
     public static void main(String[] args) {
         String banner = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
