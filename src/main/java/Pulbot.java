154f00c68a6e872be9f14bf055609a4a3f899863
Pulastya Satapathy <83450788+pulas2345@users.noreply.github.com>
2026-08-21 09:03:08 +0800

    Add echo for user commands

--- src/main/java/Pulbot.java
+++ src/main/java/Pulbot.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 /**
- * Starts Pulbot and displays its welcome banner.
+ * Starts Pulbot and echos commands entered by the user.
  */
 public class Pulbot {
+    private static final String INDENT = "    ";
+    private static final String SEPARATOR = INDENT + "_".repeat(65);
+
+    /**
+     * Runs Pulbot until the user enters "bye" command.
+     */
     public static void main(String[] args) {
-        System.out.println("=".repeat(75));
         String banner = """
                         ##### ##                ###         ##### ##
                      ######  /###                ###     ######  /##
@@ -26,10 +33,23 @@ public class Pulbot {
                     ###
                 """;
         System.out.println(banner);
-        System.out.println("Hello! I'm PulBot.");
-        System.out.println("What can I do for you?\n");
-        System.out.println("=".repeat(75));
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("=".repeat(75));
+        System.out.println(INDENT + " Hello! I'm PulBot.");
+        System.out.println(INDENT + " What can I do for you?");
+        System.out.println(SEPARATOR + "\n");
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            System.out.println(SEPARATOR);
+
+            System.out.println(INDENT + " " + input);
+            System.out.println(SEPARATOR + "\n");
+
+            if (input.equals("bye")) {
+                System.out.println(INDENT + " Bye. Hope to see you again soon!");
+                System.out.println(SEPARATOR + "\n");
+            }
+        }
+        scanner.close();
     }
 }
