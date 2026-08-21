8100b3a5fb4894661a39ee19e927c481a9728891
Pulastya Satapathy <83450788+pulas2345@users.noreply.github.com>
2026-08-21 10:48:32 +0800

    add support for task list

--- src/main/java/Pulbot.java
+++ src/main/java/Pulbot.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * Starts Pulbot and echos commands entered by the user.
+ * Starts Pulbot and stores tasks entered by the user.
  */
 public class Pulbot {
     private static final String INDENT = "    ";
@@ -34,10 +34,14 @@ public class Pulbot {
                 """;
         System.out.println(banner);
         System.out.println(INDENT + " Hello! I'm PulBot.");
-        System.out.println(INDENT + " What can I do for you?");
+        System.out.println(INDENT + " What tasks do you want to add to your list?");
+        System.out.println(INDENT + " (Type 'list' to view your list, and 'bye' to exit.)");
         System.out.println(SEPARATOR + "\n");
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
             System.out.println(SEPARATOR);
@@ -48,7 +52,16 @@ public class Pulbot {
                 break;
             }
 
-            System.out.println(INDENT + " " + input);
+            if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(INDENT + " " + (i + 1) + ". " + tasks[i]);
+                }              
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(INDENT + " added " + input + " to your list.");
+            }
+
             System.out.println(SEPARATOR + "\n");
         }
         scanner.close();
